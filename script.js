86adebea294222ed88419533cd03032c5c1fb1e1
Luke Sullivan <sully097@gmail.com>
2025-01-03 11:57:20 +0000

    Implement number validation and update UI elements

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const userInput = document.getElementById('user-input');
+const resultsDiv = document.getElementById('results-div');
 
 const validateNumber = () => {
     const input = userInput.value;
-    const regex = /([0-9])/g;
-    console.log(input.match(regex));
+
+    if (input === '') {
+        alert('Please provide a phone number');
+        return;
+    }
+
+    const regex = /\d{3}-\d{3}-\d{4}/g;
+    const isValid = input.match(regex) ? true : false;
+    console.log(isValid);
+    if (isValid) {
+        console.log(isValid);
+        resultsDiv.textContent = input.match(regex);
+        resultsDiv.style.display = 'block';
+    }
+}
+
+const clearInput = () => {
+    userInput.value = '';
+    resultsDiv.style.display = 'none'
 }
 
 checkBtn.addEventListener('click', validateNumber);
@@ -14,3 +32,5 @@ window.addEventListener('keypress', function(e) {
         validateNumber();
     }
 });
+
+clearBtn.addEventListener('click', clearInput);
